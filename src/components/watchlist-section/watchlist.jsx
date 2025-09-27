e23a7a857fb74dd8d16e30c1a248b8b6d1266c2a
Cailean Matthews <matthewsmcailean@gmail.com>
2025-09-27 18:07:43 -0400

    Updated Watchlist compnent

--- src/components/watchlist-section/watchlist.jsx
+++ src/components/watchlist-section/watchlist.jsx
@@ -5,9 +5,9 @@ function Watchlist(){
     return(
         <section className={"border-r-2 h-[94.5vh] flex flex-col"}>
             <Card name={"Apple"} symbol={"APPL"} price={182.93}></Card>
-            <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
-            <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
-            <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
+            <Card name={"Meta"} symbol={"META"} price={38.27}></Card>
+            <Card name={"JPMorgan"} symbol={"JPMG"} price={145.66}></Card>
+            <Card name={"Chase Banking"} symbol={"CHS"} price={110.12}></Card>
             <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
         </section>
     );
