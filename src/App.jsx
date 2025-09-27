72a14eb40e97e3f659717f248a34f83285938313
Cailean Matthews <matthewsmcailean@gmail.com>
2025-09-27 14:01:12 -0400

    added watchlist biolerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 // import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header.jsx'
+import Watchlist from "./components/watchlist-section/watchlist.jsx";
 
 function App() {
 
   return (
-    <>
+    <body className={"flex flex-col"}>
         <Header></Header>
-    </>
+        <main>
+            <Watchlist></Watchlist>
+        </main>
+    </body>
+
+
   )
 }
 
---
+++ src/components/watchlist-section/watchlist.jsx
@@ -0,0 +1,13 @@
+import "./watchlist.css"
+
+function Watchlist(){
+    return(
+        <section className={"border-r-2 h-[94.5vh]"}>
+            <div className={"card"}>
+                <h1>AMZN</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Watchlist;
