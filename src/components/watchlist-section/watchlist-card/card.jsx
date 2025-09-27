9bec6a32d59343f934392c07d0d8007ccaff06af
Cailean Matthews <matthewsmcailean@gmail.com>
2025-09-27 18:03:45 -0400

    added card
    card is used in the watchlist compnent and displays the symbol name and price

---
+++ src/components/watchlist-section/watchlist-card/card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card({symbol, name, price}){
+    return(
+            <div className={"flex-grow p-9.5 border-b-2 flex flex-col justify-center"}>
+                <h1 className={"text-center text-3xl font-bold"}>{symbol}</h1>
+                <h2 className={"text-center m-[0 0 1rem 0]"}>{name}</h2>
+                <h3 className={"text-center text-2xl font-semibold"}>${price}</h3>
+            </div>
+    );
+}
+
+export default Card;
--- src/components/watchlist-section/watchlist.jsx
+++ src/components/watchlist-section/watchlist.jsx
@@ -1,11 +1,14 @@
 import "./watchlist.css"
+import Card from "./watchlist-card/card.jsx"
 
 function Watchlist(){
     return(
-        <section className={"border-r-2 h-[94.5vh]"}>
-            <div className={"card"}>
-                <h1>AMZN</h1>
-            </div>
+        <section className={"border-r-2 h-[94.5vh] flex flex-col"}>
+            <Card name={"Apple"} symbol={"APPL"} price={182.93}></Card>
+            <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
+            <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
+            <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
+            <Card name={"Amazon"} symbol={"AMZN"} price={293.87}></Card>
         </section>
     );
 }
