7d23a4bf7ca19654f3b352bd5ba4c7ac8ee7cc30
Cailean Matthews <matthewsmcailean@gmail.com>
2025-10-08 15:54:33 -0400

    Initialized chart

--- src/components/watchlist-section/watchlist-card/card.jsx
+++ src/components/watchlist-section/watchlist-card/card.jsx
@@ -2,7 +2,7 @@
 
 function Card({symbol, name, price}){
     return(
-            <div className={"flex-grow p-9.5 border-b-2 flex flex-col justify-center"}>
+            <div className={"flex-grow p-3 border-b-2 flex flex-col justify-around"}>
                 <h1 className={"text-center text-3xl font-bold"}>{symbol}</h1>
                 <h2 className={"text-center m-[0 0 1rem 0]"}>{name}</h2>
                 <h3 className={"text-center text-2xl font-semibold"}>${price}</h3>
